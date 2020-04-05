98bc8413bcfb954720c58a8bdce082db93dce663
leonardo-berlatto <leoberlatto@gmail.com>
2020-04-04 21:46:16 -0300

    🎨 Changing package name (once more) to photoapp

--- service-discovery/src/main/java/br/com/photoapp/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/br/com/photoapp/servicediscovery/ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package br.com.eureka.servicediscovery;
+package br.com.photoapp.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- service-discovery/src/test/java/br/com/photoapp/servicediscovery/ServiceDiscoveryApplicationTests.java
+++ service-discovery/src/test/java/br/com/photoapp/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.eureka.servicediscovery;
+package br.com.photoapp.servicediscovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
