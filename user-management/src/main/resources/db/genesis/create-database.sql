027c4e26eef77db756bd3938c4e5b89f3f7f3adc
Leonardo Silveira Berlatto <leoberlatto@gmail.com>
2020-04-10 00:45:13 -0300

    🗃 Adding datasource config and initial scripts

---
+++ user-management/src/main/resources/db/genesis/create-database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user(
+	id INT IDENTITY PRIMARY KEY NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name  VARCHAR(50) NOT NULL,
+    email  VARCHAR(50) NOT NULL,
+	username  VARCHAR(30) NOT NULL,
+	password  VARCHAR(32) NOT NULL
+);
