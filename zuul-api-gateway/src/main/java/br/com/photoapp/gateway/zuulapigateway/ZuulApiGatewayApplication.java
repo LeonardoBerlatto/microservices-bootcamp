c6380e7db47db6a1d98ef010571524ea4638b515
Leonardo Silveira Berlatto <leoberlatto@gmail.com>
2020-05-03 23:16:23 -0300

    ✨ Adding endpoint to retrieve user info

--- zuul-api-gateway/src/main/java/br/com/photoapp/gateway/zuulapigateway/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/br/com/photoapp/gateway/zuulapigateway/ZuulApiGatewayApplication.java
@@ -2,12 +2,13 @@ package br.com.photoapp.gateway.zuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
-@SpringBootApplication
 public class ZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
