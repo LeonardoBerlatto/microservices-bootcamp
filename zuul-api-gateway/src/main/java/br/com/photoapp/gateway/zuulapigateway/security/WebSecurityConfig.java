c1a293132fe198255ec49cd08ea3430f97ad95ec
Leonardo Silveira Berlatto <leoberlatto@gmail.com>
2020-05-06 22:46:17 -0300

    🔒 Updating gateway security

--- zuul-api-gateway/src/main/java/br/com/photoapp/gateway/zuulapigateway/security/WebSecurityConfig.java
+++ zuul-api-gateway/src/main/java/br/com/photoapp/gateway/zuulapigateway/security/WebSecurityConfig.java
@@ -1,16 +1,14 @@
 package br.com.photoapp.gateway.zuulapigateway.security;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-import javax.servlet.http.HttpServletResponse;
-
-@Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(jsr250Enabled = true, prePostEnabled = true)
 public class WebSecurityConfig extends ResourceServerConfigurerAdapter {
 
     @Value("${zuul.routes.user-management-microservice.signup.path}")
@@ -23,19 +21,11 @@ public class WebSecurityConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity security) throws Exception {
         security
                 .csrf().disable()
-                // handle an authorized attempts
-                .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-                .and()
-                // Add a filter to validate the tokens with every request
-                //.addFilterAfter(jwtTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                // Authorization requests config
                 .authorizeRequests()
 
                 // Allow all who are accessing users service
                 .antMatchers(HttpMethod.POST, userSignupUrl, userLoginUrl)
                 .permitAll()
-
-                // Any other request must be authenticated
                 .anyRequest().authenticated();
     }
 }
