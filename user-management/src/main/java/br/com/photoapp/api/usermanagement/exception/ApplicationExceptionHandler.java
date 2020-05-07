fb8e1874b49dda299d7b8000936ce80a5c8d6114
Leonardo Silveira Berlatto <leoberlatto@gmail.com>
2020-05-06 22:49:02 -0300

    ✨ Adding validation to check if there is a user with same email or username

--- user-management/src/main/java/br/com/photoapp/api/usermanagement/exception/ApplicationExceptionHandler.java
+++ user-management/src/main/java/br/com/photoapp/api/usermanagement/exception/ApplicationExceptionHandler.java
@@ -4,6 +4,7 @@ import br.com.photoapp.api.usermanagement.exception.model.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,7 +18,7 @@ import java.util.Date;
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
-    @ExceptionHandler(value = Exception.class)
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessage> handleUnexpectedException(Exception exception, WebRequest request) {
         final HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 
@@ -26,7 +27,7 @@ public class ApplicationExceptionHandler {
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), httpStatus);
     }
 
-    @ExceptionHandler(value = NotFoundExeception.class)
+    @ExceptionHandler(NotFoundExeception.class)
     public ResponseEntity<ErrorMessage> handleNotFoundException(NotFoundExeception exception, WebRequest request) {
         final HttpStatus httpStatus = HttpStatus.NOT_FOUND;
 
@@ -35,8 +36,7 @@ public class ApplicationExceptionHandler {
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), httpStatus);
     }
 
-
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorMessage> handleConstraintViolationException(MethodArgumentNotValidException exception, WebRequest request) {
         final HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
 
@@ -47,6 +47,24 @@ public class ApplicationExceptionHandler {
         }
 
         final ErrorMessage errorMessage = buildMessage(exceptionMessage, request, httpStatus.value());
+
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), httpStatus);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorMessage> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception, WebRequest request) {
+        final HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        final ErrorMessage errorMessage = buildMessage("Error when reading the request body.", request, httpStatus.value());
+
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), httpStatus);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorMessage> handleBadRequestException(BadRequestException exception, WebRequest request) {
+        final HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        final ErrorMessage errorMessage = buildMessage(exception.getMessage(), request, httpStatus.value());
 
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), httpStatus);
     }
--- user-management/src/main/java/br/com/photoapp/api/usermanagement/web/controller/UserController.java
+++ user-management/src/main/java/br/com/photoapp/api/usermanagement/web/controller/UserController.java
@@ -1,10 +1,10 @@
 package br.com.photoapp.api.usermanagement.web.controller;
 
-import br.com.photoapp.api.usermanagement.domain.User;
 import br.com.photoapp.api.usermanagement.mapper.UserMapper;
 import br.com.photoapp.api.usermanagement.service.UserService;
 import br.com.photoapp.api.usermanagement.web.representation.request.CreateUserRequest;
 import br.com.photoapp.api.usermanagement.web.representation.response.UserResponse;
+import br.com.photoapp.eureka.commonservice.domain.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
