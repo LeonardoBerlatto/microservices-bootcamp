d2909cea2ebc9dbcec2f5ce84329a8da2e2f16e7
Leonardo Silveira Berlatto <leoberlatto@gmail.com>
2020-05-08 23:08:00 -0300

    ✅ Fixing tests

--- user-management/src/main/java/br/com/photoapp/api/usermanagement/security/WebSecurityConfig.java
+++ user-management/src/main/java/br/com/photoapp/api/usermanagement/security/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package br.com.photoapp.api.usermanagement.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
